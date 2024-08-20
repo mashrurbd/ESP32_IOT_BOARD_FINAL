482fd6138799054476e1dade375feb17e5e65bb6
mashrurbd <mohsinmashrur@gmail.com>
2024-08-21 03:30:17 +0600

    Updated base code.

--- src/main.cpp
+++ src/main.cpp
@@ -55,35 +55,20 @@ void lcd_i2c(int x, int y, String m)
   }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5bdd953 (updated.)
 // This function sends Arduino's up time every second to Virtual Pin (5) and shows it to the OLED Display
 void sendSensor()
 {
   // delay(2000);
 
-<<<<<<< HEAD
   //read temperature and humidity
   float t = dht.readTemperature();
 
   if (isnan(t)) 
-=======
-  // read temperature and humidity
-  float t = dht.readTemperature();
-
-  if (isnan(t))
->>>>>>> 5bdd953 (updated.)
   {
     Blynk.virtualWrite(V5, 0);
     return;
   }
-<<<<<<< HEAD
   else 
-=======
-  else
->>>>>>> 5bdd953 (updated.)
   {
     Blynk.virtualWrite(V5, t);
     return;
@@ -94,12 +79,7 @@ void sendSensor()
 void printLocalTime()
 {
   struct tm timeinfo;
-<<<<<<< HEAD
   if(!getLocalTime(&timeinfo)){
-=======
-  if (!getLocalTime(&timeinfo))
-  {
->>>>>>> 5bdd953 (updated.)
     Serial.println("Failed to obtain time");
     return;
   }
@@ -111,7 +91,6 @@ void printLocalTime()
   char dddd[3];
   char mmmm[10];
   char yyyy[5];
-<<<<<<< HEAD
   strftime(hh,3, "%I", &timeinfo);
   strftime(mm,3, "%M", &timeinfo);
   strftime(ss,3, "%S", &timeinfo);
@@ -176,66 +155,6 @@ void printLocalTime()
     
   // }
   
-=======
-  strftime(hh, 3, "%I", &timeinfo);
-  strftime(mm, 3, "%M", &timeinfo);
-  strftime(ss, 3, "%S", &timeinfo);
-  strftime(timeWeekDay, 10, "%A", &timeinfo);
-  strftime(dddd, 3, "%d", &timeinfo);
-  strftime(mmmm, 10, "%B", &timeinfo);
-  strftime(yyyy, 10, "%Y", &timeinfo);
-  int ssint = atoi(ss);
-
-  // Clear Display
-  display.clearDisplay();
-  float t = dht.readTemperature();
-
-  // Display Time
-  display.setCursor(5, 5);
-
-  if (ssint % 2 == 0)
-  {
-    // lcd.clear();
-    lcd_i2c(0, 0, "" + String(timeWeekDay) + "");
-    lcd_i2c(0, 1, "" + String(dddd) + " " + String(mmmm) + " " + String(yyyy) + "");
-    lcd_i2c(0, 2, "" + String(hh) + ":" + String(mm) + ":" + String(ss) + "");
-    lcd_i2c(0, 3, "Temp: " + String(t) + " C");
-    // bigNumberLCD.print(""+String(hh)+":"+String(mm)+":"+String(ss)+"");
-  }
-  else
-  {
-    // lcd.clear();
-    lcd_i2c(0, 0, "" + String(timeWeekDay) + "");
-    lcd_i2c(0, 1, "" + String(dddd) + " " + String(mmmm) + " " + String(yyyy) + "");
-    lcd_i2c(0, 2, "" + String(hh) + " " + String(mm) + " " + String(ss) + "");
-    lcd_i2c(0, 3, "Temp: " + String(t) + " C");
-  }
-
-  // if ((ssint > 33) || (ssint < 30))
-  // {
-
-  // display.setTextSize(2);
-  // display.print(""+String(timeWeekDay)+"");
-  // display.setCursor(5,25);
-  // display.setTextSize(1);
-  // display.print(""+String(dddd)+" "+String(mmmm)+" "+String(yyyy)+"");
-  // display.setCursor(5,45);
-  // display.setTextSize(2);
-  // if (ssint%2==0)
-  // {
-  //   display.print(""+String(hh)+":"+String(mm)+":"+String(ss)+"");
-  //   display.drawRoundRect(1,1,126,62,5,WHITE);
-  // }
-  // else
-  // {
-  //   display.print(""+String(hh)+" "+String(mm)+" "+String(ss)+"");
-  //   display.drawRoundRect(1,1,126,64,5,BLACK);
-
-  // }
-  // display.display();
-
-  // }
->>>>>>> 5bdd953 (updated.)
 }
 
 // Blynk App write listeners
